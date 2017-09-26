4912fb5e4c2987c4a089b7f177342a50635de0f0
Doyle1771 <adoyl088@uottawa.ca>
2017-09-26 13:11:01 -0400

    Update formatted_msg.java

--- formatted_msg.java
+++ formatted_msg.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 public class formatted_msg implements Serializable{
   public enum CTRL { NORMAL, TERMINATE, LOOPBACK, BROADCAST, SETUP, GET_ALL_CLIENTS};
   String dest;
@@ -11,6 +12,9 @@ public class formatted_msg implements Serializable{
   }
   public void set_terminate() { msg_ctrl = CTRL.TERMINATE; }
   public void set_loopback() { msg_ctrl = CTRL.LOOPBACK; }
+  public void set_broadcast(){msg_ctrl = CTRL.LOOPBACK;}
+  public void set_setup(){msg_ctrl = CTRL.SETUP;}
+  public void set_get_all_clients(){msg_ctrl = CTRL.GET_ALL_CLIENTS;}
   public void set_ctrl(CTRL ctrl) { msg_ctrl = ctrl; }
   public String toString(){
     String str = "formatted_msg to " + dest + " msg: " + msg;
@@ -24,7 +28,33 @@ public class formatted_msg implements Serializable{
     }
     return str;
   }
-  static formatted_msg init(formatted_msg msg){
-    return msg;
+  static formatted_msg init(){
+
+    formatted_msg msg1;
+    String dest;
+    String type;
+    String cont;
+    System.out.println("Please enter message type");
+    Scanner sc = new Scanner(System.in);
+    type=sc.nextLine();
+    System.out.println("Please enter message destination");
+    dest=sc.nextLine();
+    System.out.println("Please enter message content");
+    cont=sc.nextLine();
+    msg1= new formatted_msg(dest, cont);
+    if(type.equals("TERMINATE")){
+      msg1.set_terminate();
+    }else if(type.equals("LOOPBACK")){
+      msg1.set_loopback();
+    }else if(type.equals("BROADCAST")){
+      msg1.set_broadcast();
+    }else if(type.equals("SETUP")){
+      msg1.set_setup();
+    }else if(type.equals("GET_ALL_CLIENTS")){
+      msg1.set_get_all_clients();
+    }
+    
+    return msg1;
   };
+
 }
